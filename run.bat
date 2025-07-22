cb4b2b1a0617219747b246eada4c6d8fdaf518bb
cpduncan <cpduncan@duncanprime.com>
2025-07-22 02:53:26 -0400

    executable

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-java --module-path "C:\Users\Admin\Downloads\openjfx-24.0.2_windows-x64_bin-sdk\javafx-sdk-24.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar swedemo-1.0-SNAPSHOT.jar
+java --module-path ".\javafx-sdk-24.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar swedemo-1.0-SNAPSHOT.jar
 pause
